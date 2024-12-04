2cc9be492a50179264ca89dad5b8c3359749c9ff
OhYeHyun <ohyhohyl@naver.com>
2024-12-04 19:12:23 +0900

    feat(OrderRepository): 주문 메뉴를 저장하는 기능

---
+++ src/main/java/christmas/domain/OrderRepository.java
@@ -0,0 +1,32 @@
+package christmas.domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+public class OrderRepository {
+    private final OrderRepository instance = new OrderRepository();
+    private final List<Food> order = new ArrayList<>();
+
+    private OrderRepository() {}
+
+    public OrderRepository getInstance() {
+        return instance;
+    }
+
+    public void orderFood(Food food) {
+        order.add(food);
+    }
+
+    public boolean isExist(String name) {
+        return !order.stream()
+                .filter(food -> Objects.equals(food.getName(), name))
+                .toList()
+                .isEmpty();
+    }
+
+    public List<Food> getOrder() {
+        return Collections.unmodifiableList(order);
+    }
+}
